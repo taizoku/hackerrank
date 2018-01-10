4894ff6da8c22c9b82d3723e45bdf0bce328714b
Taiyue Tan <ryotokuro@gmail.com>
2018-01-10 23:56:03 +1100

    changed formatting to be consistent

--- helloworld.py
+++ helloworld.py
@@ -4,5 +4,5 @@ input_string = input()
 # Print a string literal saying "Hello, World." to stdout.
 print('Hello, World.')
 
-print(input_string)
 # TODO: Write a line of code here that prints the contents of input_string to stdout.
+print(input_string)
