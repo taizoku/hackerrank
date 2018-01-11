9ca143f55170d59466c8c658ab8f2f1db0b6ffdc
Taiyue Tan <ryotokuro@gmail.com>
2018-01-11 21:40:36 +1100

    takes int, double, string and adds to default params

---
+++ dataTypes.py
@@ -0,0 +1,13 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Takes input and stores it in a variable
+integer = input("Enter a int: ")
+double = input("Enter a double: ")
+string = input("Enter a string: ")
+
+# Converts inputs into respective types and adds them to initial parameters
+print(str(i + int(integer)))
+print(str(d + float(double)))
+print(s + string)
