5b730baf53b1d8b7280a8e670259da3f2eef1753
Taiyue Tan <ryotokuro@gmail.com>
2018-01-16 22:49:24 +1100

    daily program in works

---
+++ 30day/review.py
@@ -0,0 +1,35 @@
+test_cases = int(input())
+
+strings = []
+for i in range(test_cases):
+    strings.append(list(input()))
+    print(strings)
+
+even = ""
+odd = ""
+i = 0
+
+
+for i in range(test_cases):
+    strings[i]
+'''
+for string in strings:
+    if i % 2 == 0:
+        even += string[i]
+        print(string)
+    else:
+        odd += string[i]
+    i += 1
+'''
+
+# even_string = " ".join(even_list)
+# odd_string = " ".join(odd_list)
+
+print(even + " " + odd)
+
+'''
+i = 0
+for string in strings:2
+
+print(even + " " + odd)
+'''
--- random/attack.py
+++ random/attack.py
@@ -1,7 +1,15 @@
+from .classes.enemy import Enemy
+
 import random
 
 
+enemy = Enemy(200, 60)
+print("HP:", enemy.get_hp())
+
+'''
 class Enemy:
+    hp = 200
+
     def __init__(self, lowAttack, hiAttack):
         self.lowAttack = lowAttack
         self.hiAttack = hiAttack
@@ -9,6 +17,15 @@ class Enemy:
     def get_attack(self):
         print("Base damage is:", self.lowAttack)
 
+    def get_hp(self):
+        print("HP is: ", self.hp)
+
 
 enemy1 = Enemy(10, 15)
 enemy1.get_attack()
+enemy1.get_hp()
+
+enemy2 = Enemy(1, 5)
+enemy2.get_attack()
+enemy2.get_hp()
+'''
