5920f2be4cf9d66e0befd0094e358672401d1530
Taiyue Tan <ryotokuro@gmail.com>
2018-01-17 23:54:50 +1100

    added string implementation

--- 30day/arrays.py
+++ 30day/arrays.py
@@ -6,3 +6,10 @@ import sys
 n = int(input().strip())
 arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
 
+string = ""
+for i in range(n):
+    string += str(arr[n-1])
+    if n != 0:
+        string += " "
+    n -= 1
+print(string)
