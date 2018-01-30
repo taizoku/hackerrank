5a88e1b8e89bd9597a28676aee9b0f1094c8eec3
Taiyue Tan <ryotokuro@gmail.com>
2018-01-31 02:27:25 +1100

    added trivial in progress sln

--- 30day/2dArrays.py
+++ 30day/2dArrays.py
@@ -2,40 +2,48 @@
 
 import sys
 
-
 arr = []
 for arr_i in range(6):
-   arr_t = [int(arr_temp) for arr_temp in input().strip().split(' ')]
-   arr.append(arr_t)
+    arr_t = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+    arr.append(arr_t)
 
-j = 0
-while j < 6:
-    print(arr[j])
-    j += 1
+printer = 0
+while printer < 6:
+    print(arr[printer])
+    printer += 1
 
-j = 0
+across = down = 0
 rowCounter = colCounter = 1
 calculationSum = maxSum = 0
 
-read = input()
-if read == exit:
-    sys.exit
+while down < 4:
+    i = down
 
-for rows in arr:
-    calculationSum += rows[j]
+    while across < 4:
+        j = across
+        iterations = 0
 
-    print("sum is", calculationSum)
-    if calculationSum > maxSum:
-        maxSum = calculationSum
+        while iterations < 6:
+            calculationSum += arr[i][j]
 
-    #for 3
-    if colCounter < 4:
-        calculationSum = 0
+            print("Sum is", "arr[", i, "][", j, "] =", calculationSum)
+            if calculationSum > maxSum:
+                maxSum = calculationSum
 
-        if rowCounter % 2 != 0 and colCounter % 2 != 0: # if counter is not even
-            j += 1
+            if rowCounter % 2 != 0 and colCounter < 4:
+                j += 1
+                colCounter += 1
 
-        else:
-            j = 1
+            else:
+                j -= 1
+                i += 1
+                rowCounter += 1
 
-    colCounter += 1
+            if colCounter == 3:
+                rowCounter = 2
+                colCounter = 2
+
+            iterations += 1
+        across += 1
+    down += 1
+print("Maximum Sum is:", maxSum)
