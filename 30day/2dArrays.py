9ec6393d8344e630f4d4e97cd48112f65c3cef05
Taiyue Tan <ryotokuro@gmail.com>
2018-01-30 22:56:19 +1100

    still thinking

--- 30day/2dArrays.py
+++ 30day/2dArrays.py
@@ -8,16 +8,34 @@ for arr_i in range(6):
    arr_t = [int(arr_temp) for arr_temp in input().strip().split(' ')]
    arr.append(arr_t)
 
-i = j = 0
-col = counter = 1
+j = 0
+while j < 6:
+    print(arr[j])
+    j += 1
+
+j = 0
+rowCounter = colCounter = 1
 calculationSum = maxSum = 0
-for numbers in arr:
-    calculationSum += arr[i][j]
-    if counter % 2 != 0: # if counter is not even
-        i += 2
-        counter = 0
 
-    else:
-        j += 1
+read = input()
+if read == exit:
+    sys.exit
 
-    counter += 1
+for rows in arr:
+    calculationSum += rows[j]
+
+    print("sum is", calculationSum)
+    if calculationSum > maxSum:
+        maxSum = calculationSum
+
+    #for 3
+    if colCounter < 4:
+        calculationSum = 0
+
+        if rowCounter % 2 != 0 and colCounter % 2 != 0: # if counter is not even
+            j += 1
+
+        else:
+            j = 1
+
+    colCounter += 1
