cef4d0a81d782564c8d7edbb16f7d02e529cb135
Taiyue Tan <ryotokuro@gmail.com>
2018-01-30 22:17:56 +1100

    added some thoughts

--- 30day/2dArrays.py
+++ 30day/2dArrays.py
@@ -7,3 +7,17 @@ arr = []
 for arr_i in range(6):
    arr_t = [int(arr_temp) for arr_temp in input().strip().split(' ')]
    arr.append(arr_t)
+
+i = j = 0
+col = counter = 1
+calculationSum = maxSum = 0
+for numbers in arr:
+    calculationSum += arr[i][j]
+    if counter % 2 != 0: # if counter is not even
+        i += 2
+        counter = 0
+
+    else:
+        j += 1
+
+    counter += 1
