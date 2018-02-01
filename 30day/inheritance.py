be701dc5b27145ddfcdc3e3d74a84e5182676dc3
Taiyue Tan <ryotokuro@gmail.com>
2018-02-01 23:53:19 +1100

    added basic structure for class

---
+++ 30day/inheritance.py
@@ -0,0 +1,16 @@
+class Student(Person):
+# Class Constructor
+#
+#   Parameters:
+#   firstName - A string denoting the Person's first name.
+#   lastName - A string denoting the Person's last name.
+#   id - An integer denoting the Person's ID number.
+#   scores - An array of integers denoting the Person's test scores.
+#
+# Write your constructor here
+
+
+#   Function Name: calculate
+#   Return: A character denoting the grade.
+#
+# Write your function here
