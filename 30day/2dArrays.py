45edc45e5f1d3dc7840865f6fa439dcb8fe943e2
Taiyue Tan <ryotokuro@gmail.com>
2018-02-01 22:17:57 +1100

    broken for negative inputs

--- 30day/2dArrays.py
+++ 30day/2dArrays.py
@@ -2,49 +2,56 @@
 
 import sys
 
+
+# Just prints the input array to stdout
+def print_array():
+    printer = 0
+    while printer < 6:
+        print(arr[printer])
+        printer += 1
+
+
 arr = []
 for arr_i in range(6):
     arr_t = [int(arr_temp) for arr_temp in input().strip().split(' ')]
     arr.append(arr_t)
 
-printer = 0
-while printer < 6:
-    print(arr[printer])
-    printer += 1
+# print_array()
 
 down = 0
-maxSum = 0
+calculationSum = maxSum = 0
 
 while down < 4:
     across = 0
     while across < 4:
+        maxSum = max(maxSum, calculationSum)
         iterations = 0
-        i = down
-        print("Down count is:", down)
-        j = across
-        print("Across count is:", across)
+        x = down
+        # print("Down count is:", down)
+        y = across
+        # print("Across count is:", across)
 
         rowCounter = colCounter = 0
         calculationSum = 0
         while iterations < 7:
-            calculationSum += arr[i][j]
+            calculationSum += arr[x][y]
+            '''
             print("rowCounter is now:", rowCounter)
             print("colCounter is now:", colCounter)
-            print("Sum is", "arr[", i, "][", j, "] =", calculationSum)
-            if calculationSum > maxSum:
-                maxSum = calculationSum
-
-            if rowCounter == 0 and colCounter == 2 or rowCounter == 1:
-                i += 1
-                j -= 1
+            print("Sum is", "arr[", x, "][", y, "] =", calculationSum)
+            '''
+            if (rowCounter == 0 and colCounter == 2) or (rowCounter == 1):
+                x += 1
+                y -= 1
                 rowCounter += 1
                 colCounter -= 1
 
             else:
-                j += 1
+                y += 1
                 colCounter += 1
 
             iterations += 1
         across += 1
     down += 1
-print("Maximum Sum is:", maxSum)
+# print("Maximum Sum is:", maxSum)
+print(maxSum)
