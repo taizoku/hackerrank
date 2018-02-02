949a4c6dacede32c27bb8cbef3628bcb24bd8feb
Taiyue Tan <ryotokuro@gmail.com>
2018-02-02 17:55:23 +1100

    added grading program for student database

--- 30day/inheritance.py
+++ 30day/inheritance.py
@@ -1,4 +1,16 @@
+class Person:
+    def __init__(self, firstName, lastName, idNumber):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.idNumber = idNumber
+
+    def printPerson(self):
+        print("Name:", self.lastName + ",", self.firstName)
+        print("ID:", self.idNumber)
+
+
 class Student(Person):
+    super()
 # Class Constructor
 #
 #   Parameters:
@@ -6,15 +18,37 @@ class Student(Person):
 #   lastName - A string denoting the Person's last name.
 #   id - An integer denoting the Person's ID number.
 #   scores - An array of integers denoting the Person's test scores.
-#
-    Student():
-
+#   DEFAULT CONSTRUCTOR
+    scores = []
+    def __init__(self, firstName, lastName, id, scores):
+        print("A student has been created!")
 
 
 #   Function Name: calculate
 #   Return: A character denoting the grade.
 #
-    char calculate():
+    def calculate(self, scores):
+        average = 0
+        grade = ''
+        for result in scores:
+            average += result
+        average /= len(scores)
+
+        if 90 <= average <= 100:
+            grade = 'O'
+        elif 80 <= average < 90:
+            grade = 'E'
+        elif 70 <= average < 80:
+            grade = 'A'
+        elif 55 <= average < 70:
+            grade = 'P'
+        elif 40 <= average < 55:
+            grade = 'D'
+        else:
+            grade = 'T'
+
+        return grade
+
 
 line = input().split()
 firstName = line[0]
