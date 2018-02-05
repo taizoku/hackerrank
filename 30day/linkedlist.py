210e9d77023abe99d22ca3b4a69a1f5463116673
Taiyue Tan <ryotokuro@gmail.com>
2018-02-05 23:57:56 +1100

    added day15 linked list proj

---
+++ 30day/linkedlist.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+class Solution:
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
