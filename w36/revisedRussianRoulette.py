61c3861d9b7fe5c5ad49aa5c7aa7d9f11aec37cf
Taiyue Tan <ryotokuro@gmail.com>
2018-02-07 23:48:13 +1100

    added file changes

---
+++ w36/revisedRussianRoulette.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+import sys
+
+def revisedRussianRoulette(doors):
+    # Complete this function
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    doors = list(map(int, input().strip().split(' ')))
+    result = revisedRussianRoulette(doors)
+    print (" ".join(map(str, result)))
