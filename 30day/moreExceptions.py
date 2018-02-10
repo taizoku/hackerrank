6ab72efaafd330f0f8e5555dc9ebdb7ec3ae8514
Taiyue Tan <ryotokuro@gmail.com>
2018-02-10 20:45:20 +1100

    added input method

--- 30day/moreExceptions.py
+++ 30day/moreExceptions.py
@@ -3,3 +3,14 @@ class Calculator:
         if n < 0 or p < 0:
             print("n and p should be non-negative")
         return pow(n, p)
+
+
+myCalculator = Calculator()
+T = int(input())
+for i in range(T):
+    n, p = map(int, input().split())
+    try:
+        ans = myCalculator.power(n, p)
+        print(ans)
+    except Exception as e:
+        print(e)
