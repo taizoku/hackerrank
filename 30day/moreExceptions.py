ae10714e544b19e88dca8d8a7d7ae39da7dbe915
Taiyue Tan <ryotokuro@gmail.com>
2018-02-11 21:33:43 +1100

    throw exception is raise in python

--- 30day/moreExceptions.py
+++ 30day/moreExceptions.py
@@ -1,7 +1,8 @@
 class Calculator:
     def power(self, n, p):
         if n < 0 or p < 0:
-            print("n and p should be non-negative")
+            #throw/raise exception
+            raise ValueError("n and p should be non-negative")
         return pow(n, p)
 
 
