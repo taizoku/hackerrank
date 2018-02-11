d3452949ef1684886f771611475514dbfd29e4b5
Taiyue Tan <ryotokuro@gmail.com>
2018-02-11 23:31:22 +1100

    stack implementation

--- 30day/stacks.py
+++ 30day/stacks.py
@@ -1,1 +1,29 @@
 class Solution:
+    # class entry
+
+    # read the string s
+    s = input()
+    # Create the Solution class object
+    obj = Solution()
+
+    l = len(s)
+    # push/enqueue all the characters of string s to stack
+    for i in range(l):
+        obj.pushCharacter(s[i])
+        obj.enqueueCharacter(s[i])
+
+    isPalindrome = True
+    '''
+    pop the top character from stack
+    dequeue the first character from queue
+    compare both the characters
+    '''
+    for i in range(l // 2):
+        if obj.popCharacter() != obj.dequeueCharacter():
+            isPalindrome = False
+            break
+    # finally print whether string s is palindrome or not.
+    if isPalindrome:
+        print("The word, " + s + ", is a palindrome.")
+    else:
+        print("The word, " + s + ", is not a palindrome.")
