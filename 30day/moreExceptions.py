94dfca00401f26554c8268d92910f33c3022880f
Taiyue Tan <ryotokuro@gmail.com>
2018-02-12 21:18:26 +1100

    using super/Exception constructor to print

--- 30day/moreExceptions.py
+++ 30day/moreExceptions.py
@@ -1,15 +1,15 @@
 class Calculator:
     def power(self, n, p):
         # raise ValueError("n and p should be non-negative")
-
         if n < 0 or p < 0:
             raise NegativeException
         return pow(n, p)
 
 
 class NegativeException(Exception):
-    def __init__(self):
-        print("n and p should be non-negative")
+    # def __init__(self):
+        # print("n and p should be non-negative")
+    super().__init__("n and p should be non-negative")
 
 
 myCalculator = Calculator()
