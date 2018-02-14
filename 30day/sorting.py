92e5a1ce3671efa1a806e98ca08ad6e2e6670652
Taiyue Tan <ryotokuro@gmail.com>
2018-02-14 23:53:59 +1100

    bubble sorting working on it

--- 30day/sorting.py
+++ 30day/sorting.py
@@ -2,3 +2,10 @@ n = int(input().strip())
 a = list(map(int, input().strip().split(' ')))
 
 # bubble sort
+numSwaps = temp = 0
+prevNum = 32678
+for element in a:
+    temp = element
+    if element < prevNum:
+        element = prevNum
+    prevNum = element
