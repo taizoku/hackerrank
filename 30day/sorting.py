5ff86df71cd8807906d0998d3204ca459cf53691
Taiyue Tan <ryotokuro@gmail.com>
2018-02-15 00:15:21 +1100

    issue: out of range

--- 30day/sorting.py
+++ 30day/sorting.py
@@ -2,16 +2,17 @@ n = int(input().strip())
 a = list(map(int, input().strip().split(' ')))
 
 # bubble sort
-numSwaps = temp = j = 0
+numSwaps = temp = 0
 print(a)
 
-for a in range(0, n):
-    if a[j] > a[j+1]:
-        temp = a[j]
-        a[j] = a[j+1]
-        a[j+1] = temp
+for i in range(0, n):
+    j = i + 1
+    if a[i] < a[j]:
+        temp = a[i]
+        a[i] = a[j]
+        a[j] = temp
         numSwaps += 1
+
 firstElement = a[0]
 lastElement = a[-1]
-
-print("first", firstElement, "last", lastElement)
+print("First:", firstElement, "Last:", lastElement)
