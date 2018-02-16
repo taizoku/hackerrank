9138f91a51013600778c47edd5e9e50380f7c4e6
Taiyue Tan <ryotokuro@gmail.com>
2018-02-16 15:08:23 +1100

    adding generics file/templates (For c++)

---
+++ 30day/generics.cpp
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/30-generics/problem
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+/**
+*    Name: printArray
+*    Print each element of the generic vector on a new line. Do not return anything.
+*    @param A generic vector
+**/
+
+T printArray(T array[]) {
+
+}
