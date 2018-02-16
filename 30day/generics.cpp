2c6a5228df93c3727e252f9476d30a6a402b1945
Taiyue Tan <ryotokuro@gmail.com>
2018-02-16 15:22:35 +1100

    template vector printer works (Generic)

--- 30day/generics.cpp
+++ 30day/generics.cpp
@@ -12,6 +12,35 @@ using namespace std;
 *    @param A generic vector
 **/
 
-T printArray(T array[]) {
+template <class T>
 
+void printArray(vector<T> &t_vector) {
+	for(T element : t_vector) {
+		cout << element << endl;
+	}
+}
+
+int main() {
+	int n;
+	
+	cin >> n;
+	vector<int> int_vector(n);
+	for (int i = 0; i < n; i++) {
+		int value;
+		cin >> value;
+		int_vector[i] = value;
+	}
+	
+	cin >> n;
+	vector<string> string_vector(n);
+	for (int i = 0; i < n; i++) {
+		string value;
+		cin >> value;
+		string_vector[i] = value;
+	}
+
+	printArray<int>(int_vector);
+	printArray<string>(string_vector);
+
+	return 0;
 }
