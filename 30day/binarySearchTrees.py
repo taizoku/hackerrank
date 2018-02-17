7d8d0aefe4598cf31cb1dd3491a319c27308f5fa
Taiyue Tan <ryotokuro@gmail.com>
2018-02-17 23:55:06 +1100

    classes adding

--- 30day/binarySearchTrees.py
+++ 30day/binarySearchTrees.py
@@ -3,3 +3,16 @@ class Node:
         self.right=self.left=None
         self.data = data
 
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
