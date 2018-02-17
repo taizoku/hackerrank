51b7ef7f2f4c36565264ddf469e6d1fa08b6e556
Taiyue Tan <ryotokuro@gmail.com>
2018-02-17 23:57:39 +1100

    formatting fixing

--- 30day/binarySearchTrees.py
+++ 30day/binarySearchTrees.py
@@ -1,28 +1,32 @@
 class Node:
-    def __init__(self,data):
-        self.right=self.left=None
+    def __init__(self, data):
+        self.right = self.left = None
         self.data = data
 
+
 class Solution:
-    def insert(self,root,data):
-        if root==None:
+    def insert(self, root, data):
+        if root is None:
             return Node(data)
         else:
-            if data<=root.data:
-                cur=self.insert(root.left,data)
-                root.left=cur
+            if data <= root.data:
+                cur = self.insert(root.left, data)
+                root.left = cur
             else:
-                cur=self.insert(root.right,data)
-                root.right=cur
+                cur = self.insert(root.right, data)
+                root.right = cur
         return root
 
-# my class
+    def getHeight(self, root):
+        # return something
+        return root
 
-T=int(input())
-myTree=Solution()
-root=None
+
+T = int(input())
+myTree = Solution()
+root = None
 for i in range(T):
-    data=int(input())
-    root=myTree.insert(root,data)
-height=myTree.getHeight(root)
-print(height)       
+    data = int(input())
+    root = myTree.insert(root, data)
+height = myTree.getHeight(root)
+print(height)
