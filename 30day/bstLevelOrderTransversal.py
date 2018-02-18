b2efbe6d468712c998fcb1a89089a18c54717797
Taiyue Tan <ryotokuro@gmail.com>
2018-02-18 18:03:32 +1100

    node implementation added

---
+++ 30day/bstLevelOrderTransversal.py
@@ -0,0 +1,19 @@
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+    
