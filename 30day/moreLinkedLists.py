ed95c055334a1e033054c5f766cc974642bd73fa
Taiyue Tan <ryotokuro@gmail.com>
2018-02-26 23:38:54 +1100

    added class solution

---
+++ 30day/moreLinkedLists.py
@@ -0,0 +1,18 @@
+class Solution:
+    def insert(self,head,data):
+            p = Node(data)
+            if head==None:
+                head=p
+            elif head.next==None:
+                head.next=p
+            else:
+                start=head
+                while(start.next!=None):
+                    start=start.next
+                start.next=p
+            return head
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
