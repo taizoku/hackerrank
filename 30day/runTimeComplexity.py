604318be9a426d266ec0374452fe5126f632a57f
Taiyue Tan <ryotokuro@gmail.com>
2018-03-04 23:42:34 +1100

    input method for prime checker

---
+++ 30day/runTimeComplexity.py
@@ -0,0 +1,19 @@
+# https://www.hackerrank.com/challenges/30-running-time-and-complexity/problem
+
+'''
+Background:
+A prime is a natural number > 1 that has no positive divisors other than 1 and itself.
+Given a number, , determine and print whether it's prime or not prime.
+
+Input:
+The first line contains an integer, T, the number of test cases.
+Each of the T subsequent lines contains an integer, n, to be tested for primality.
+
+Output:
+For each test case, print whether n is Prime or Not prime on a new line.
+'''
+
+# Take in number of test cases and inputs
+testCases = int(input())
+for i in range(testCases):
+    int(input())
