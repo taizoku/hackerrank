5c06ab09f1c52e6521fc883f7d8ba4c335f6312a
ryotokuro <ryotokuro@gmail.com>
2018-03-06 23:40:57 +1100

    might use a map

---
+++ 30day/nestedLogic.py
@@ -0,0 +1,23 @@
+'''
+# Task
+Your local library needs your help! Given the expected and actual return dates for a library book,
+    create a program that calculates the fine (if any). The fee structure is as follows:
+
+1. If the book is returned on or before the expected return date, no fine will be charged (i.e.: .
+2. If the book is returned after the expected return day but still within the same calendar month
+     and year as the expected return date, .
+3. If the book is returned after the expected return month but still within the same calendar year
+     as the expected return date, the .
+4. If the book is returned after the calendar year in which it was expected, there is a fixed
+     fine of .
+'''
+
+input = {}
+for i in range(0, 5):
+    input = input()
+input[returnDay] = input[0]
+input[returnMonth] = input[1]
+input[returnYear] =
+input[expectedDay] =
+input[expectedMonth] =
+input[expectedYear]
