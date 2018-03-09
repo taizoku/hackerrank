f519db910416d087e9b1cfad1f7ff45cbd697650
ryotokuro <ryotokuro@gmail.com>
2018-03-09 23:50:54 +1100

    cancellation of class conditions

--- 30day/testing.py
+++ 30day/testing.py
@@ -1,6 +1,18 @@
-condition = False
+'''
+# need at least 5 test cases
+# Output:
+    YES
+    NO
+    YES
+    NO
+    YES
+'''
+
+
+cancellation = False
+if n < k:
+    cancellation = True
 if t <= 5:
     if 3 <= n <= 200:
         if 1 <= k <= n:
-            condition = True
-            
+            cancellation = True
