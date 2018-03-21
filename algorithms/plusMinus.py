5421b6c381e35b71075ef4604355db5c25b27590
ryotokuro <ryotokuro@gmail.com>
2018-03-21 23:45:26 +1100

    added input method and conditionals for functions

---
+++ algorithms/plusMinus.py
@@ -0,0 +1,16 @@
+def plusMinus(arr):
+    size = len(arr)
+    # if positive
+
+    # if negative
+
+    # if zeros
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
+    
