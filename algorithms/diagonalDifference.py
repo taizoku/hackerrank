8a3d2c681ddd8dfa7fa4f74907193d2666b0014f
ryotokuro <ryotokuro@gmail.com>
2018-03-22 22:56:40 +1100

    using os environment and input methods for array

---
+++ algorithms/diagonalDifference.py
@@ -0,0 +1,21 @@
+import os
+
+def diagonalDifference(a):
+    print("diagonal calculations go here")
+
+
+if __name__ == '__main__':
+    f = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    a = []
+
+    for _ in range(n):
+        a.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(a)
+
+    f.write(str(result) + '\n')
+
+    f.close()
