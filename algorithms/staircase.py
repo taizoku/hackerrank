3bcd378e15d3163561884f789fced84d1dc823c6
ryotokuro <ryotokuro@gmail.com>
2018-03-23 23:20:13 +1100

    help me bub attacking me

--- algorithms/staircase.py
+++ algorithms/staircase.py
@@ -1,5 +1,13 @@
 def staircase(n):
-    print(n)
+    i = 1
+    for i in range(n):
+        print()
+        print(" ")
+    print("#")
+
+    return n
+    #for i in range(n):
+        # if(n-1) > 0:
 
 
 if __name__ == '__main__':
