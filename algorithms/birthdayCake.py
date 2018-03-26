e3d604a88375cacfe7e76d04fd262fdb718aeac0
ryotokuro <ryotokuro@gmail.com>
2018-03-27 00:01:24 +1100

    sorting algorithm that shows which is good not tested yet

--- algorithms/birthdayCake.py
+++ algorithms/birthdayCake.py
@@ -3,8 +3,10 @@ n = int(input())
 ar = list(map(int, input().rstrip().split()))
 
 def birthdayCakeCandles(n, ar):
-    print("Cake!")
-    yes = n-1
-    sorted(ar)
+    canReach = 0
+    for candle in sorted(ar):
+        if candle == n-1:
+            canReach += 1
+    return canReach
 
 print(birthdayCakeCandles(n, ar))
