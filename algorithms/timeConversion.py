67a64729da02d0e6ea355e120491994cdbc521d6
ryotokuro <ryotokuro@gmail.com>
2018-03-28 00:43:45 +1100

    should be working now forgot the first stuff

--- algorithms/timeConversion.py
+++ algorithms/timeConversion.py
@@ -1,6 +1,7 @@
 def timeConversion(s):
+    twentyFour = s[:-2]
     if s[-2:] == "PM":
-        twentyFour = str(int(s[:2]) + 12) + s[2:]
+        twentyFour = str(int(s[:2]) + 12) + s[2:-2]
     return twentyFour
 
 if __name__ == '__main__':
