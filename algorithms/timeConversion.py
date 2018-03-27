6f4c60405ae0f191c55efd2f7814f94aec580d9c
ryotokuro <ryotokuro@gmail.com>
2018-03-28 00:53:27 +1100

    confusion between midnight <am> and noon <pm>

--- algorithms/timeConversion.py
+++ algorithms/timeConversion.py
@@ -1,11 +1,14 @@
 def timeConversion(s):
     twentyFour = s[:-2]
     if s[-2:] == "PM":
+        if s[:2] != "12":
+            twentyFour = str(int(s[:2]) + 12) + s[2:-2]
+
+    else:
         if s[:2] == "12":
             twentyFour = "00" + s[2:-2]
-        else:
-            twentyFour = str(int(s[:2]) + 12) + s[2:-2]
     return twentyFour
+
 
 if __name__ == '__main__':
     s = input()
