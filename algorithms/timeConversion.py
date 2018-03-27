e74367bd77e06e2c4404351d6f75ebcaa35ed275
ryotokuro <ryotokuro@gmail.com>
2018-03-28 00:37:28 +1100

    slicing was incorrect, still issue with assignment

--- algorithms/timeConversion.py
+++ algorithms/timeConversion.py
@@ -1,9 +1,8 @@
 def timeConversion(s):
-    print(s[:2])
     if s[-2:] == "PM":
         twelve = str(int(s[:2]) + 12)
         print("change", twelve)
-        # s[:1] = twelve
+        s[:1] = twelve
     return s
 
 if __name__ == '__main__':
