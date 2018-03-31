dd50da4ccd8fe0ebdff9962a1307a01679337cd5
ryotokuro <ryotokuro@gmail.com>
2018-04-01 00:26:48 +1100

    misstep with 5 and multiples

--- algorithms/grading.py
+++ algorithms/grading.py
@@ -4,7 +4,12 @@ import os
 
 def gradingStudents(grades):
     for grade in grades:
-        rounded = grade + abs(5 - int(str(grade)[1]))
+        print(grade + abs(5 - int(str(grade)[1])))
+        if int(str(grade)[1]) < 5:
+            rounded = grade + 5 - int(str(grade)[1])
+
+        elif int(str(grade)[1]) > 5:
+            rounded = grade + 10 - int(str(grade)[1])
             # if grade(1) > 2 and grade(1) < 5 or grade(1) > 7 and grade(1) < 10:
         if rounded - grade < 3:
             grade = rounded
