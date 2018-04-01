ea7f49c176b089660c972aa8e5fc3bbb506607e7
ryotokuro <ryotokuro@gmail.com>
2018-04-02 09:39:50 +1000

    input method and stuff

---
+++ algorithms/appleOrange.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+#
+# Complete the countApplesAndOranges function below.
+#
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    #
+    # Write your code here.
+    #
+
+if __name__ == '__main__':
+    st = input().split()
+
+    s = int(st[0])
+
+    t = int(st[1])
+
+    ab = input().split()
+
+    a = int(ab[0])
+
+    b = int(ab[1])
+
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    apple = list(map(int, input().rstrip().split()))
+
+    orange = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apple, orange)
