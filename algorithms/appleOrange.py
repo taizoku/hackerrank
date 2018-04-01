83da9417934f8cae4b869b7612fb81889787bd9a
ryotokuro <ryotokuro@gmail.com>
2018-04-02 09:52:30 +1000

    prev issue with for loop iterator

--- algorithms/appleOrange.py
+++ algorithms/appleOrange.py
@@ -1,15 +1,17 @@
 def countApplesAndOranges(s, t, a, b, apples, oranges):
     appleCount = orangeCount = 0
-    house = range(s, t)
-    print(house)
+    house = list(range(s, t))
 
     for apple in apples:
-        if a + apples in house:
+        if a + apple in house:
             appleCount += 1
 
     for orange in oranges:
         if b + orange in house:
             orangeCount += 1
+
+    print(appleCount)
+    print(orangeCount)
 
 if __name__ == '__main__':
     st = input().split()
