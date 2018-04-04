d136fcb9ff3a118d30243e6f2b84f62244581658
ryotokuro <ryotokuro@gmail.com>
2018-04-04 23:59:44 +1000

    Figuring out

--- algorithms/kangaroo.py
+++ algorithms/kangaroo.py
@@ -1,5 +1,5 @@
 def kangaroo(x1, v1, x2, v2):
-    # Complete this function
+    if x1 / time == 0
 
 x1, v1, x2, v2 = input().strip().split(' ')
 x1, v1, x2, v2 = [int(x1), int(v1), int(x2), int(v2)]
