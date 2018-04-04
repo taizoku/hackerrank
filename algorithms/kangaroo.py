17f6fba2c6f29f1de0af5cba6b18bdd68a51b937
ryotokuro <ryotokuro@gmail.com>
2018-04-04 23:52:45 +1000

    input method defined

---
+++ algorithms/kangaroo.py
@@ -0,0 +1,7 @@
+def kangaroo(x1, v1, x2, v2):
+    # Complete this function
+
+x1, v1, x2, v2 = input().strip().split(' ')
+x1, v1, x2, v2 = [int(x1), int(v1), int(x2), int(v2)]
+result = kangaroo(x1, v1, x2, v2)
+print(result)
