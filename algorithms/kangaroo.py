1bd18434f5d29b3d81dd696331bc5fd5a45466f5
ryotokuro <ryotokuro@gmail.com>
2018-04-05 00:01:53 +1000

    perhaps int test?

--- algorithms/kangaroo.py
+++ algorithms/kangaroo.py
@@ -1,5 +1,12 @@
 def kangaroo(x1, v1, x2, v2):
-    if x1 / time == 0
+    if x1 > x2:
+       if x1 / v2 == 0:
+            True
+
+    else:
+        if x2 / v1 == 0:
+            True
+
 
 x1, v1, x2, v2 = input().strip().split(' ')
 x1, v1, x2, v2 = [int(x1), int(v1), int(x2), int(v2)]
