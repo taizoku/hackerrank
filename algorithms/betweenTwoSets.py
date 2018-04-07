e6f84e12819a05a74730266887f4e046cf84a2fa
ryotokuro <ryotokuro@gmail.com>
2018-04-07 22:43:03 +1000

    input method defined etc

---
+++ algorithms/betweenTwoSets.py
@@ -0,0 +1,23 @@
+def getTotalX(a, b):
+    #
+    # Write your code here.
+    #
+
+if __name__ == '__main__':
+    f = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(a, b)
+
+    f.write(str(total) + '\n')
+
+    f.close()
