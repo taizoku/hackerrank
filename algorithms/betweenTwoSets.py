aac174d0e0fd9f3cba7c62febf5f7e18e6c94b8e
ryotokuro <ryotokuro@gmail.com>
2018-04-09 23:49:24 +1000

    some progress

--- algorithms/betweenTwoSets.py
+++ algorithms/betweenTwoSets.py
@@ -1,14 +1,18 @@
 def getTotalX(a, b):
     a = sorted(a)
     b = sorted(b)
+    stuff = []
     # all factors of the integer being considered
     for first in a:
-        for second in b:
-            for i in range(a[1] ,b[-1]):
-                
-                if second % stuff != 0:
-                    remove element lol
-            print("mod func or something")
+        for number in range(a[1], b[-1]):
+            if number % first:
+                stuff.append(number)
+
+    for second in b:
+        if second % stuff != 0:
+            bye elem    ent
+
+    return len(stuff)
 
 if __name__ == '__main__':
     f = open(os.environ['OUTPUT_PATH'], 'w')
