ff8ef98b947a54aa291fbee3352e697730d94235
ryotokuro <ryotokuro@gmail.com>
2018-04-11 22:33:40 +1000

    fixed output methods and stuff still some bugs brb

--- algorithms/breakingRecords.py
+++ algorithms/breakingRecords.py
@@ -12,16 +12,14 @@ def breakingRecords(score):
             lowest = game
             countLow += 1
 
-if __name__ == '__main__':
-    f = open(os.environ['OUTPUT_PATH'], 'w')
+    return countHigh, countLow
 
+
+if __name__ == '__main__':
     n = int(input())
 
     score = list(map(int, input().rstrip().split()))
 
     result = breakingRecords(score)
 
-    f.write(' '.join(map(str, result)))
-    f.write('\n')
-
-    f.close()
+    print(result)
