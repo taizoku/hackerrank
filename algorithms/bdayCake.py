a7b4ba20ce447ec9378309b3e53e30f5d72e3dba
ryotokuro <ryotokuro@gmail.com>
2018-04-12 23:52:09 +1000

    base layout of code

---
+++ algorithms/bdayCake.py
@@ -0,0 +1,9 @@
+def solve(n, s, d, m):
+    # Complete this function
+
+n = int(input().strip())
+s = list(map(int, input().strip().split(' ')))
+d, m = input().strip().split(' ')
+d, m = [int(d), int(m)]
+result = solve(n, s, d, m)
+print(result)
