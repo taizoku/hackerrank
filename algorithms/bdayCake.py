23b444728ca088e83711cd74c67f4d75e12464f8
ryotokuro <ryotokuro@gmail.com>
2018-04-13 22:35:24 +1000

    defining my plan on how to tackle the program

--- algorithms/bdayCake.py
+++ algorithms/bdayCake.py
@@ -1,9 +1,22 @@
 def solve(n, s, d, m):
     validCombination = sum = 0
+    s = sorted(s)
+    # s = [2 2 1 3 2]
+
+    # s = [1 2 2 2 3]
+    # remove counts of same number twice
+
+    # s = [1 2 2 3]
+
+    # check if sum elements (2) == 4
+    # add to counter
+
     for i in range(n):
         sum += s[i]
+        # m is how many numbers we are adding
+
+        # d is the unique sum we want to reach using unique combinations
         if len(sum) == d:
-            if sum == m:
                 validCombination += 1
     return validCombination
 
