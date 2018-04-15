12efbb62da0d1ed7ce0cafe5dae8ccc2e1e20048
ryotokuro <ryotokuro@gmail.com>
2018-04-15 23:52:56 +1000

    function base layout and input method defined

--- algorithms/divisibleSumPairs.py
+++ algorithms/divisibleSumPairs.py
@@ -6,3 +6,12 @@
 #   find and print number of (i,j) pairs where
 #   i < j and a[i] + a[j] is divisble by k
 
+def divisibleSumPairs(n, k, ar):
+    # Complete this function
+
+
+n, k = input().strip().split(' ')
+n, k = [int(n), int(k)]
+ar = list(map(int, input().strip().split(' ')))
+result = divisibleSumPairs(n, k, ar)
+print(result)
