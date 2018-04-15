bff4dddf1f074d313eb9c76a4af2dbc8e21acd14
ryotokuro <ryotokuro@gmail.com>
2018-04-16 00:13:44 +1000

    hm iterators should be changed i want j to be like i+1 initially then increase from there

--- algorithms/divisibleSumPairs.py
+++ algorithms/divisibleSumPairs.py
@@ -6,10 +6,13 @@
 #   find and print number of (i,j) pairs where
 #   i < j and a[i] + a[j] is divisble by k
 
+
 def divisibleSumPairs(n, k, ar):
-    for i in ar:
-        if i < j and ar[i] + ar[j] % k == 0:
-            add to counter
+    for i in range(n):
+        for j in range(1, n):
+            print(i,j)
+        # if i < j and ar[i] + ar[j] % k == 0:
+        # add to counter
 
 
 n, k = input().strip().split(' ')
