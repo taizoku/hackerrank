7596ba3b869b745c7a4b8a297ae2d66cc8ecd611
ryotokuro <ryotokuro@gmail.com>
2018-04-26 22:04:51 +1000

    major bug issues with double checking and such and such

--- algorithms/betweenTwoSets.py
+++ algorithms/betweenTwoSets.py
@@ -1,16 +1,32 @@
 def getTotalX(a, b):
     a = sorted(a)
     b = sorted(b)
+
     satisfied = []
 
-    for number in range(a[-1], b[0]):
+    for number in range(a[-1], b[0] + 1):
         for first in a:
             if number % first == 0:
+                print(first, number)
+                if first == a[-1]:
+                    # for second in b:
+                    satisfied.append(number)
+            else:
+                break
+
+
+
+
+        #for first in a:
+           # if number % first == 0:
+
                 # should be factor of all in b
-                for second in b:
-                    if second % number == 0:
-                        if first == a[-1] and second == b[-1]:
-                            satisfied.append(number)
+                #for second in b:
+                   # print(second, number)
+                   # print(second, "%", number, "is", second % number)
+                    #if second % number == 0:
+                      #  if first == a[-1] and second == b[-1]:
+                           # satisfied.append(number)
 
     print(satisfied)
     return len(satisfied)
