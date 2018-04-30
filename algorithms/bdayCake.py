010d32426123cabb959dc42521f8d02a1108dbdb
ryotokuro <ryotokuro@gmail.com>
2018-04-30 21:01:48 +1000

    still bugs with arrays of 1 and storing used permutations

--- algorithms/bdayCake.py
+++ algorithms/bdayCake.py
@@ -1,67 +1,67 @@
-def solve(n, s, d, m):
-    # s = [2 2 1 3 2]
+def solve(numofSquares, array, desiredValue, addLength):
     usedSegment = []
-    #s = sorted(s)
-    # s = [1 2 2 2 3]
-    print(s)
-    # remove counts of same number twice
-    # s = [1 2 2 3]
-
-    # variable keeps track of where we are in the maybe??
-
-    # should be in range 0 to (n-(m-1)); m being length equal to birth month
-    for i in range(n-(m-1)):
-        segmentValue = 0
-        # range from 0 to m (length we adding)
-        for limit in range(m):
-
-        if segmentValue == d:
-            usedSegment.append(...)
-            result += 1
-
-        return result
-
-        # check if segment not already checked
-        if (s[i], to s[i+m]) not in usedSegment:
-
-            if tracking < n:
-                while i < m:
-                    if s[i] == d:
-                        usedSegment.append(,)
-
-
-
-    new = True
-    onlyTwo = []
-    for element in s:
-        if element not in onlyTwo:
-            new = False
-            check = element
-            onlyTwo.append(element)
-
-        else:
-            if new is False:
-                new = True
-                onlyTwo.append((element))
-
-            if element == check:
-                continue
-    print(onlyTwo)
 
     validCombination = 0
 
-    # m is how many numbers we are adding
-    for i in range(len(onlyTwo)):
-        for j in range(1, len(onlyTwo)):
-            sum = 0
-            sum = onlyTwo[i] + onlyTwo[j]
-            print(onlyTwo[i], "+", onlyTwo[j], "=", sum)
-            # d is the unique sum we want to reach using unique combinations
-            if sum == d:
-                validCombination += 1
+    # should be in range 0 to (n-(m-1)); m being length equal to birth month
+    for i in range(numofSquares):
+        if array[i + (addLength - 1)] == None:
+            break
+
+        currentSegment = []
+        segmentValue = 0
+        # range from 0 to m (length we adding)
+        for limit in range(addLength):
+            segmentValue += array[i + limit]
+            currentSegment[i].append(array[i + limit])
+
+        if (segmentValue == desiredValue) and currentSegment[i] not in usedSegment:
+            usedSegment.append(currentSegment[i])
+            #usedSegment.append([array[i], array[i + 1], array[i + 2]])
+            validCombination += 1
+
+    print(usedSegment)
 
     return validCombination
 
+    #  # check if segment not already checked
+    #     if (s[i], to s[i+m]) not in usedSegment:
+    #
+    #         if tracking < n:
+    #             while i < m:
+    #                 if s[i] == d:
+    #                     usedSegment.append(,)
+    #
+    #
+    #
+    # new = True
+    # onlyTwo = []
+    # for element in s:
+    #     if element not in onlyTwo:
+    #         new = False
+    #         check = element
+    #         onlyTwo.append(element)
+    #
+    #     else:
+    #         if new is False:
+    #             new = True
+    #             onlyTwo.append((element))
+    #
+    #         if element == check:
+    #             continue
+    # print(onlyTwo)
+    #
+    # validCombination = 0
+    #
+    # # m is how many numbers we are adding
+    # for i in range(len(onlyTwo)):
+    #     for j in range(1, len(onlyTwo)):
+    #         sum = 0
+    #         sum = onlyTwo[i] + onlyTwo[j]
+    #         print(onlyTwo[i], "+", onlyTwo[j], "=", sum)
+    #         # d is the unique sum we want to reach using unique combinations
+    #         if sum == d:
+    #             validCombination += 1
 
 n = int(input().strip())
 s = list(map(int, input().strip().split(' ')))
