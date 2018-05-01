63378c4d2108a5d04faa9268ef927f190ca0103e
ryotokuro <ryotokuro@gmail.com>
2018-05-02 02:28:11 +1000

    added some if statements and started writing some conditions etc to determine month and days

--- algorithms/dayOfTheProgrammer.py
+++ algorithms/dayOfTheProgrammer.py
@@ -1,5 +1,3 @@
-import sys
-
 def solve(year):
     print("Year..")
     # 1700 to 1917 use the Julian Calendar
@@ -14,6 +12,30 @@ def solve(year):
     #   Gregorian Calendar: leap years:
     #       divisible by 400
     #       divisible by 4 not divisible by 100
+
+    # Given a year find the date of the 256th day
+    # print format dd.mm.yyyy
+
+    # Note:
+    # Can't be October, November or December
+    # 30 days: April June September
+    # 31 days: Jan, March, May, July, August,
+
+    if year < 1918:
+
+    elif year == 1918:
+        # if it's a leap year
+        if year % 400 == 0 and year % 4 == 0 and year % 100 != 0:
+            # 29 days in feb
+            # feb 14 is 32
+            # feb 29 is 47th day
+        month = 2
+        (day, month, year)
+
+        else:
+
+
+
 
 
 year = int(input().strip())
