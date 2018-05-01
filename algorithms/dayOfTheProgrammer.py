a976858a127f4a908f45b835fccc37d05e55081b
ryotokuro <ryotokuro@gmail.com>
2018-05-02 02:52:01 +1000

    added isLeapYear function lol

--- algorithms/dayOfTheProgrammer.py
+++ algorithms/dayOfTheProgrammer.py
@@ -1,41 +1,64 @@
+def isLeapYear(year):
+    leapYear = False
+    if year < 1918:
+        if year % 4:
+            leapYear = True
+
+    else:
+        if year % 400 == 0 and year % 4 == 0 and year % 100 != 0:
+            leapYear = True
+
+    return leapYear
+
 def solve(year):
-    print("Year..")
-    # 1700 to 1917 use the Julian Calendar
-    # 1918 Transition from Julian->Gregorian
-    #   Next day after January 31st was Feb 14
-    #   in 1918 Feb 14 was the 32nd day of the year
-    # 1919 onwards they use the Gregorian Calendar
-    # For both calendars February was the variable months
-    #   leap year 29 days
-    #   other year 28 days
+    # BACKGROUND
+    # 1700 to 1917 - Julian Calendar
+    # 1918 - Julian -> Gregorian
+    # Feb 14 was the 32nd day of the year (comes after Jan 31)
+    # 1919 onwards - Gregorian Calendar
+
+    # FEBRUARY is the variable month
+    #   LEAP year = 29 days
+    #   NOT LEAP year = 28 days
+
+    # LEAP YEAR RULES
     #   Julian Calendar: leap years divisible by 4
     #   Gregorian Calendar: leap years:
-    #       divisible by 400
-    #       divisible by 4 not divisible by 100
+    #       - divisible by 400
+    #       - divisible by 4 NOT divisible by 100
 
+    # NOTE
+    # CAN'T BE October, November or December > 256 days
+    # 30 days:
+    #   - 4 (April)
+    #   - 6 (June)
+    #   - 9 (September)
+    # 31 days:
+    #   - 3 (March)
+    #   - 5 (May)
+    #   - 7 (July)
+    #   - 8 (August)
+
+    # TASK
     # Given a year find the date of the 256th day
-    # print format dd.mm.yyyy
-
-    # Note:
-    # Can't be October, November or December
-    # 30 days: April June September
-    # 31 days: Jan, March, May, July, August,
+    #   print format dd.mm.yyyy
 
     if year < 1918:
 
     elif year == 1918:
         # if it's a leap year
-        if year % 400 == 0 and year % 4 == 0 and year % 100 != 0:
+        if isLeapYear(year):
             # 29 days in feb
             # feb 14 is 32
             # feb 29 is 47th day
-        month = 2
+        month = 3
+        for i in range(48, 256):
+            month == 3:
+            month += 1
+
         (day, month, year)
 
         else:
-
-
-
 
 
 year = int(input().strip())
