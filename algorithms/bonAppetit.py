696667a9958187e6dc1b8a29205dc6a1569ae2c3
ryotokuro <ryotokuro@gmail.com>
2018-05-03 23:58:19 +1000

    added input/output descriptions and extra info + example

--- algorithms/bonAppetit.py
+++ algorithms/bonAppetit.py
@@ -3,4 +3,23 @@
 #   - anna wont pay for that
 # brian gets the check and calculates Anna's portion
 
-# 
+# assume bill = [2, 4, 6]
+# anna refuses to eat item k = bill[2] which costs 6
+# brian calculates that anna will pay (2+4)/2 = 3
+# including cost of bill[2] it is (2 + 4 + 6)/2 = 6
+#   in case 2 he should refund 3 to Anna
+
+# input format:
+# space separated integers
+#   - n: num of items ordered
+#   - k: index of item anna didn't eat
+
+# bill[i] array
+
+# integer b - amount of money brian charged Anna for share of bill
+
+# output:
+# if fair print "Bon Appetit"
+# else print amount needed to refund
+
+
