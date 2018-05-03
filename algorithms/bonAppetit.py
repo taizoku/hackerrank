231dd011a424dd7a3ba3237b605c6b9b7077b794
ryotokuro <ryotokuro@gmail.com>
2018-05-04 00:45:49 +1000

    fixed input methods, fixing bugs so compilable, notEaten is the index actually, should be in range(int) cant iterate over int.. etc

--- algorithms/bonAppetit.py
+++ algorithms/bonAppetit.py
@@ -9,31 +9,37 @@
 # including cost of bill[2] it is (2 + 4 + 6)/2 = 6
 #   in case 2 he should refund 3 to Anna
 
-def checkAppetit():
-    result = charged - actual
-
-    result = "Bon Appetit"
-
-    return result
-
-# input format:
-# space separated integers
-#   - n: num of items ordered
-#   - k: index of item anna didn't eat
-n, k = input()
-
-# bill[i] array
-bill = []
-for i in range(n):
-    bill[i] = input()
-
-# integer b - amount of money brian charged Anna for share of bill
-b = int(input())
-
-print(checkAppetit())
-
 # output:
 # if fair print "Bon Appetit"
 # else print amount needed to refund
 
 
+def checkAppetit(numOfItems, notEaten, bill, amountCharged):
+    print(bill)
+    shouldBeCharged = 0
+    bill.remove(bill[notEaten])
+
+    for i in range(numOfItems):
+        shouldBeCharged += bill[i]
+
+    shouldBeCharged /= 2
+    result = amountCharged - shouldBeCharged
+
+    result = "Bon Appetit"
+
+    return result
+
+
+# input format:
+# space separated integers
+#   - n: num of items ordered
+#   - k: index of item anna didn't eat
+n, k = map(int, input().strip().split(' '))
+
+# bill[i] array
+bill = list(map(int, input().strip().split(' ')))
+
+# integer b - amount of money brian charged Anna for share of bill
+b = int(input())
+
+print(checkAppetit(n, k, bill, b))
