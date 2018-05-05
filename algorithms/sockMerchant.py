75ec98ea0786c8c524b7b754f6265f649684aa71
ryotokuro <ryotokuro@gmail.com>
2018-05-05 13:19:58 +1000

    problem outlined in the comments

---
+++ algorithms/sockMerchant.py
@@ -0,0 +1,9 @@
+# pile of socks to PAIR by COLOUR
+
+# input:
+#   - n: number of socks
+#   - ar: array of colours of socks
+
+# task:
+# determine how many pairs of socks with matching colours there are
+
