4cff38390ab7b5bb51c7f312c91a4a1b53a9e908
ryotokuro <ryotokuro@gmail.com>
2018-05-05 13:41:36 +1000

    index out of range hmm

--- algorithms/sockMerchant.py
+++ algorithms/sockMerchant.py
@@ -13,14 +13,13 @@ def sock_merchant(num_of_socks, sock_pile):
     i = num_of_pairs = 0
     while i < num_of_socks:
         print("i", i)
-        if sock_pile[i+1] is not None:
-            if sock_pile[i] == sock_pile[i+1]:
-                num_of_pairs += 1
-                i += 2
-                print(i)
+        if sock_pile[i] == sock_pile[i+1]:
+            num_of_pairs += 1
+            i += 2
+            print(i)
 
-            else:
-                i += 1
+        else:
+            i += 1
         # if sock_pile[i] == sock_pile[i+1]:
         #     sock_pile.pop(i)
         #     sock_pile.pop(i + 1)
