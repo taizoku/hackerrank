63d3730b0ef3a2c76d152115333660ef37a9b584
ryotokuro <ryotokuro@gmail.com>
2018-05-05 13:44:14 +1000

    SOLVED: Working solution, removed in progress prints and compiled successfully against tests

--- algorithms/sockMerchant.py
+++ algorithms/sockMerchant.py
@@ -7,12 +7,12 @@
 # task:
 # determine how many pairs of socks with matching colours there are
 
+
 def sock_merchant(num_of_socks, sock_pile):
     sock_pile = sorted(sock_pile)
 
     i = num_of_pairs = 0
     while i < num_of_socks - 1:
-        print("i", i)
         if sock_pile[i] == sock_pile[i+1]:
             num_of_pairs += 1
             i += 2
@@ -20,11 +20,6 @@ def sock_merchant(num_of_socks, sock_pile):
 
         else:
             i += 1
-        # if sock_pile[i] == sock_pile[i+1]:
-        #     sock_pile.pop(i)
-        #     sock_pile.pop(i + 1)
-        #     num_of_pairs += 1
-        #     i += 1
 
     return num_of_pairs
 
