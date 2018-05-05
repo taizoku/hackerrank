fe1012ce634c0e2ebbfea5240eba804c9e9f6f13
ryotokuro <ryotokuro@gmail.com>
2018-05-05 13:28:08 +1000

    idea of method implementation added

--- algorithms/sockMerchant.py
+++ algorithms/sockMerchant.py
@@ -8,7 +8,15 @@
 # determine how many pairs of socks with matching colours there are
 
 def sock_merchant(num_of_socks, sock_pile):
+    sock_pile = sorted(sock_pile)
+
     num_of_pairs = 0
+    for i in range(num_of_socks):
+        if sock_pile[i] == sock_pile[i+1]:
+            sock_pile.pop(i, i+1)
+            num_of_pairs += 1
+            i += 1
+
     return num_of_pairs
 
 
