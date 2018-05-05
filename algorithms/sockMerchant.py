a73518ff9aa5048a029b9bbaeb14dabd7ccf9be1
ryotokuro <ryotokuro@gmail.com>
2018-05-05 13:24:59 +1000

    function inputs defined and fixed up

--- algorithms/sockMerchant.py
+++ algorithms/sockMerchant.py
@@ -7,11 +7,12 @@
 # task:
 # determine how many pairs of socks with matching colours there are
 
-def sock_merchant():
+def sock_merchant(num_of_socks, sock_pile):
     num_of_pairs = 0
     return num_of_pairs
+
 
 n = int(input())
 arr = map(int, input().strip().split(' '))
 
-print(sock_merchant())
+print(sock_merchant(n, arr))
