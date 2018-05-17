69b22ade8bec429e8a41720dcb289aaec1638f91
ryotokuro <ryotokuro@gmail.com>
2018-05-17 23:16:09 +1000

    input method defined etc

--- algorithms/countingValleys.py
+++ algorithms/countingValleys.py
@@ -13,3 +13,6 @@
 # find number of valleys walked through
 # e.g. s = [D D U U U U D D] first enters valley 2 units deep then climbs mountain 2 units high
 
+n = int(input())
+
+s = list(input().strip().split(''))
