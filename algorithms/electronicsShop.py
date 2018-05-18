5d2f41cd2aaaa72431299f69b8c971e3e1f20242
ryotokuro <ryotokuro@gmail.com>
2018-05-19 00:28:41 +1000

    problem outlined in comments

---
+++ algorithms/electronicsShop.py
@@ -0,0 +1,9 @@
+# monica wants to spend as much money as she can
+# if she doesn't have enough for BOTH print -1
+
+# e.g. s = 60
+# keyboards = [40, 50, 60]
+# usb drive = [5,  8,  12]
+# possible to purchase 40k + 12usb = 52
+# or higher is then 50k + 8usb = 58
+
