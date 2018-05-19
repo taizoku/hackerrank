848098493aba8fb024a696d06afcd4e6741e44e8
ryotokuro <ryotokuro@gmail.com>
2018-05-20 00:12:22 +1000

    inputs and outputs outlined

--- algorithms/catsAndAMouse.py
+++ algorithms/catsAndAMouse.py
@@ -4,3 +4,12 @@
 #   - if arrive at same time
 #       mouse can move and escapes while they fight
 
+# given q queries in form x, y, z
+#   x - position cat A
+#   y - position cat B
+#   z - position mouse
+
+# output:
+# if cat a wins: print "Cat A"
+# if cat b wins: print "Cat B"
+# if both same: print "Mouse C"
