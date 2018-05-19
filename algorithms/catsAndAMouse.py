8d8b0e1f1d3b094f2597ad0a324e98d32ec145b7
ryotokuro <ryotokuro@gmail.com>
2018-05-20 00:09:16 +1000

    problem background added

---
+++ algorithms/catsAndAMouse.py
@@ -0,0 +1,6 @@
+# two cats and a mouse at position on a line
+# determine which cat reaches the mouse first
+# cats travel at equal speed
+#   - if arrive at same time
+#       mouse can move and escapes while they fight
+
