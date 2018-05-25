51c65aaec9f6a73c54468adc496a0cffb3daf166
ryotokuro <ryotokuro@gmail.com>
2018-05-25 23:11:35 +1000

    simple explanation of problem

--- algorithms/magicSquare.py
+++ algorithms/magicSquare.py
@@ -1,4 +1,5 @@
 # define magic square to be:
-#   - n*n matrix of distinct postive integers
+#   - n*n matrix of distinct positive integers
 
-# For example, we start with the following matrix
+# Replace any number a in the 3x3 matrix with b
+#   - at minimal cost abs(a-b)
