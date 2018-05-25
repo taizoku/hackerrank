e5df834e831fda24bea6a83484089a6bef19ddf4
ryotokuro <ryotokuro@gmail.com>
2018-05-25 23:58:04 +1000

    looking into problem

--- algorithms/magicSquare.py
+++ algorithms/magicSquare.py
@@ -4,10 +4,19 @@
 # Replace any number a in the 3x3 matrix with b
 #   - at minimal cost abs(a-b)
 
-# On closer observation, and looking at the example problems, looks like they only replace one number in the row
-#    - might be a good place to start!
+# Realised it's actually just how little I can change and still have a magic number at MIN COST
+    # MAGIC NUMBER is 15
 
 def formingMagicSquare(square):
+    print(square)
+    # loop goes through row, col
+    for i in range(3):
+        for j in range(3):
+            print(square[i][j])
+            # rules
+            # check row, col, diagonal is 15
+    square[0][0]
+
     return square
 
 
