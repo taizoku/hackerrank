f829fdbb13c39600ae9781efa79f2df0183e015f
ryotokuro <ryotokuro@gmail.com>
2018-05-25 23:16:46 +1000

    input method defined for magic square entry

--- algorithms/magicSquare.py
+++ algorithms/magicSquare.py
@@ -3,3 +3,12 @@
 
 # Replace any number a in the 3x3 matrix with b
 #   - at minimal cost abs(a-b)
+
+# On closer observation, and looking at the example problems, looks like they only replace one number in the row
+#    - might be a good place to start!
+
+s = []
+
+for _ in range(3):
+    s.append(list(map(int, input().strip().split())))
+    
