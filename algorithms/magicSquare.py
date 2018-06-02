23634b0bddabdc9de3175834ca13a537e7d7b180
ryotokuro <ryotokuro@gmail.com>
2018-06-02 23:43:26 +1000

    more checks for magic square in diagonal should be like 8 combinations

--- algorithms/magicSquare.py
+++ algorithms/magicSquare.py
@@ -5,18 +5,26 @@
 #   - at minimal cost abs(a-b)
 
 # Realised it's actually just how little I can change and still have a magic number at MIN COST
-    # MAGIC NUMBER is 15
+# check row, col, diagonal is 15
 
 def formingMagicSquare(square):
+    # [0,0][0,1][0,2]
+    # [1,0][1,1][1,2]
+    # [2,0][2,1][2,2]
     print(square)
+
     # loop goes through row, col
     for i in range(3):
         for j in range(3):
             print(square[i][j])
-            # rules
-            # check row, col, diagonal is 15
-    square[0][0] + square[0][1] + square[0][2] == 15
 
+
+    # first horizontal
+    square[0][0] + square[0][1] + square[0][2] == 15
+    # first vertical
+    square[0][0] + square[1][0] + square[1][2] == 15
+    # first diagonal down
+    square[0][0] + square[1][1] + square[2][2] == 15
 
     return square
 
