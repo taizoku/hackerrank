d0b91d2826d058aa45a6dc04db30922a4c8c11bd
ryotokuro <ryotokuro@gmail.com>
2018-06-07 23:51:09 +1000

    input and more problem description added

--- algorithms/pickingNumbers.py
+++ algorithms/pickingNumbers.py
@@ -1,3 +1,13 @@
 # Given array of numbers
 # - find max num of integers you can select from array such that the absolute
 # > difference between the any two of the chosen are less than or equal to 1
+
+# Example
+# - array = [1, 1, 2, 2, 4, 4, 5, 5, 5]
+# - can create TWO sub-arrays
+# 1. [1, 1, 2, 2] length = 4
+# 2. [4, 4, 5, 5, 5] length = 5
+
+# INPUT
+# 1. single integer n (size of array)
+# 2. n space separated integers a[i]
