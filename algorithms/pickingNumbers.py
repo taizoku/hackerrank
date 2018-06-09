b791fb27bd7b2a93a692f6e9d3fdf8555c3b1aa5
ryotokuro <ryotokuro@gmail.com>
2018-06-09 23:55:30 +1000

    dont really want to create multiple arrays, also only need to check unique numbers, not all permutations really

--- algorithms/pickingNumbers.py
+++ algorithms/pickingNumbers.py
@@ -8,10 +8,14 @@
 # 1. [1, 1, 2, 2] length = 4
 # 2. [4, 4, 5, 5, 5] length = 5
 def pickingNumbers(array):
+    print("array:", array)
     subArrays = []
     for i in range(len(array)):
-        subArrays.append(array[i])
-    print(subArrays)
+        for j in range(1, len(array)):
+            if array[i] not in subArrays:
+                subArrays.append(array[i])
+
+    print("subarray:", subArrays)
     return len(array)
 
 
