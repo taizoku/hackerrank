d789ac6bcac872f6a461d540ed2c43b80771cd6d
ryotokuro <ryotokuro@gmail.com>
2018-07-08 20:03:52 +1000

    described input and outputs desired

--- algorithms/pdfViewer.py
+++ algorithms/pdfViewer.py
@@ -2,3 +2,12 @@
 # Using the letter heights given,
 # determine the area of the rectangle highlight in mm^2
 # - assuming width of letters are all 1mm wide
+
+# input format
+# 1. 26 space separated integers
+#   - describes heights of each letter [a-z]
+
+# 2. single word made of lowercase english alphabetic letters
+
+# output format
+# single integer denoting area in mm^2
