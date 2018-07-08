7b522dc85981a320f264c8e1c7119f5230958dee
ryotokuro <ryotokuro@gmail.com>
2018-07-08 20:02:07 +1000

    problem description added

---
+++ algorithms/pdfViewer.py
@@ -0,0 +1,4 @@
+# given a list of letter heights in the alphabet and a string.
+# Using the letter heights given,
+# determine the area of the rectangle highlight in mm^2
+# - assuming width of letters are all 1mm wide
