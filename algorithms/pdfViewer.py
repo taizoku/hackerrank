c32a3f7f827e510f4527d8d44d7038102e15e9d5
ryotokuro <ryotokuro@gmail.com>
2018-07-08 20:46:09 +1000

    using maps is difficult, might just use simple arithmetic

--- algorithms/pdfViewer.py
+++ algorithms/pdfViewer.py
@@ -6,6 +6,19 @@
 # input format
 # 1. 26 space separated integers
 #   - describes heights of each letter [a-z]
+
+def designerPdfViewer(alphaHeights, word):
+    area = 0
+    width = len(word)
+
+    # height is highest letter height
+    for i in range(len(word)+1):
+        word[0] = alphaHeights[[0]]
+
+    height = word[0]
+
+    return width*height
+
 noLabel = list(map(int, input().strip().split(' ')))
 
 alphaHeights = {}
@@ -14,10 +27,14 @@ for letter in range(ord('a'), ord('z')+1):
     alphaHeights[chr(letter)] = noLabel[i]
     i += 1
 
+# or no storing required ord(letter) - ord('a')
+
 print(alphaHeights)
 
 # 2. single word made of lowercase english alphabetic letters
 word = input()
+
+print(designerPdfViewer(alphaHeights, word))
 
 # output format
 # single integer denoting area in mm^2
