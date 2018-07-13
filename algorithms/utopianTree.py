d69088947496fc09162e2982795222a802240244
ryotokuro <ryotokuro@gmail.com>
2018-07-14 01:54:52 +1000

    defined method for increasing and returning height

--- algorithms/utopianTree.py
+++ algorithms/utopianTree.py
@@ -3,7 +3,24 @@
 # Each summer, its height increases by 1 meter.
 
 # Laura plants a Utopian Tree sapling with a height of 1 meter at the onset of spring.
-# How tall will her tree be after  growth cycles?
+# How tall will her tree be after n growth cycles?
+
+
+def utopianTree(numCycles):
+    height = 1
+    for i in range(numCycles):
+        # spring is every odd n
+        if i % 2 != 0:
+            # height doubles
+            height *= height
+
+        # summer for every even
+        else:
+            # height increases by 1
+            height += 1
+
+    return height
+
 
 # INPUT:
 # The first line contains an integer, t, the number of test cases.
