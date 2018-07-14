16f7a09e8a22741da30935c6d95ec77dcf713833
ryotokuro <ryotokuro@gmail.com>
2018-07-14 17:48:48 +1000

    problem description, input wanted added

---
+++ algorithms/angryProfessor.py
@@ -0,0 +1,15 @@
+# A Discrete Mathematics professor has a class of students.
+# Frustrated with their lack of discipline,
+# he decides to cancel class if fewer than some number of students are present when class starts.
+# Arrival times go from on time (arrivalTime <= 0) to arrived late (arrivalTime > 0).
+
+# INPUT FORMAT
+# 1. t - number of test cases
+# * Each test case consists of TWO lines
+
+# First line has two SPACE-SEPARATED INTEGERS:
+# - n, number of students in class
+# - k, cancellation threshold
+
+# Second line contains n space-separated integers (a_1, ... a_n)
+# - describes arrival times for each student
