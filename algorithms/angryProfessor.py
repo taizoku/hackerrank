807357c95a97246fb09da37049b8301305fd70f8
ryotokuro <ryotokuro@gmail.com>
2018-07-14 18:53:43 +1000

    added link to problem, tryin to develop consistent format across my code

--- algorithms/angryProfessor.py
+++ algorithms/angryProfessor.py
@@ -1,3 +1,4 @@
+# HACKER RANK CHALLENGE: https://www.hackerrank.com/challenges/angry-professor/problem
 # A Discrete Mathematics professor has a class of students.
 # Frustrated with their lack of discipline,
 # he decides to cancel class if fewer than some number of students are present when class starts.
