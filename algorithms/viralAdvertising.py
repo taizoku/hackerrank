50c7bb36a417381cafa9fb069cdb9e7f10bb6fa9
ryotokuro <ryotokuro@gmail.com>
2018-07-17 20:46:03 +1000

    added problem description

---
+++ algorithms/viralAdvertising.py
@@ -0,0 +1,18 @@
+# HackerLand Enterprise is adopting a new viral advertising strategy.
+# When they launch a new product, they advertise it to exactly 5 people on social media.
+
+# On the FIRST day, HALF of those 5 people
+# *(ie floor(5/2) = 2)
+# like it and share with 3 of their friends
+
+# At the beginning of the SECOND day
+# floor(5/2) * 3 = 2*3 = 6 people have received the advertisement
+
+# EACH day, floor(recipients/2) of the recipients like the advertisement and will share with 3 friends the following day
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Assuming nobody receives the advertisement twice,
+# Determine how many people have liked the ad by the end of a given day, beginning with launch day as day 1.
+
+# ----------------------------------------------------------------------------------------------------------------------
