3eb67bedfea6bd5b619952a31c19ea6d1fe1b00e
ryotokuro <ryotokuro@gmail.com>
2018-07-20 23:21:57 +1000

    added problem description

---
+++ algorithms/saveThePrisoner.py
@@ -0,0 +1,11 @@
+# A jail has a number of prisoners and a number of treats to pass out to them.
+# Their jailer decides the fairest way to divide the treats is to seat the prisoners
+# around a circular table in sequentially numbered chairs.
+
+# A chair number will be drawn from a hat.
+# Beginning with the prisoner in that chair,
+# one candy will be handed to each prisoner sequentially around the table until all have been distributed.
+
+# The jailer is playing a little joke, though.
+# The last piece of candy looks like all the others, but it tastes awful.
+# Determine the chair number occupied by the prisoner who will receive that candy.
