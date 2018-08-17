99b0d37c2bcd39a0d7007ba325c253c5f0c03352
ryotokuro <ryotokuro@gmail.com>
2018-08-17 12:54:39 +1000

    problem added

---
+++ algorithms/sequenceEquation.py
@@ -0,0 +1,6 @@
+# You are given a sequence of  integers:
+# p(1), p(2), ..., p(n)
+# Each element in the sequence is distinct and satisfies 1 <= p(x) <= n
+
+# PROBLEM
+# For each x where 1 <= x <= n, find any integer y such that p(p(y)) === x and print value y on a new line
