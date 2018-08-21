8b117615730a223dc76549b8f723ed98b48f4b76
ryotokuro <ryotokuro@gmail.com>
2018-08-21 21:56:05 +1000

    added example

--- algorithms/jumpingOnClouds.py
+++ algorithms/jumpingOnClouds.py
@@ -11,3 +11,8 @@
 
 # Given the values of n, k, and the configuration of the clouds as an array c,
 # - Can you determine the final value of e after the game ends?
+
+# EXAMPLE
+# For example, give c = [0, 0, 1, 0] and k = 2, the indices of her path are 0 -> 2 -> 0.
+# Her energy level reduces by 1 for each jump to 98. She landed on one thunderhead
+# at an additional cost of 2 energy units. Her final energy level is 96.
