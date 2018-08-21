e2800dcf62748798b8e2b5556942e045de0e3544
ryotokuro <ryotokuro@gmail.com>
2018-08-21 21:44:50 +1000

    Added problem background

---
+++ algorithms/jumpingOnClouds.py
@@ -0,0 +1,5 @@
+# BACKGROUND
+# Aerith is playing a cloud hopping game. In this game, there are sequentially
+# numbered clouds that can be thunderheads or cumulus clouds.
+# Her character must jump from cloud to cloud until it reaches the start again.
+
