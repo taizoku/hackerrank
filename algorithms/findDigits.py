a634051b6554a17c4b5ff4cbb6beaa9cbc0d42fd
ryotokuro <ryotokuro@gmail.com>
2018-08-24 22:43:18 +1000

    added note and function description

--- algorithms/findDigits.py
+++ algorithms/findDigits.py
@@ -1,6 +1,15 @@
 # BACKGROUND
 # An integer d is a divisor of an integer n if the remainder of n / d = 0.
 
-# PROBLEM   
+# PROBLEM
 # Given an integer, for each digit that makes up the integer determine whether it is a divisor.
 # Count the number of divisors occurring within the integer.
+
+# NOTE
+# Each digit is considered to be unique, so each occurrence of the same digit should be counted
+# (e.g. for n = 111, 1 is a divisor of 111 each time it occurs so the answer is 3).
+
+# FUNCTION DESCRIPTION
+# It should return an integer representing the number of digits of d that are divisors of d.
+# findDigits has the following parameter(s):
+#   - n: an integer to analyze
