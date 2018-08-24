143fa437c051814ff9f35dd07f4859a91b064ac8
ryotokuro <ryotokuro@gmail.com>
2018-08-24 22:40:44 +1000

    added background and problem

---
+++ algorithms/findDigits.py
@@ -0,0 +1,6 @@
+# BACKGROUND
+# An integer d is a divisor of an integer n if the remainder of n / d = 0.
+
+# PROBLEM   
+# Given an integer, for each digit that makes up the integer determine whether it is a divisor.
+# Count the number of divisors occurring within the integer.
