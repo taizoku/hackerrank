36d49ecdf31a0becbdcc25e1792508ada74d458e
ryotokuro <ryotokuro@gmail.com>
2018-08-28 23:59:10 +1000

    added function description

--- algorithms/extraLongFactorials.py
+++ algorithms/extraLongFactorials.py
@@ -10,3 +10,8 @@
 #   - calculate 30 * 29 * 28 * ... * 2 * 1
 #   - we get: 265252859812191058636308480000000
 
+# FUNCTION DESCRIPTION
+# Complete the extraLongFactorials function in the editor below. It should print the result and return.
+
+# extraLongFactorials has the following parameter(s):
+#   - n: an integer
