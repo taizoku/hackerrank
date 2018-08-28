32bdecc9f98d94fc736bdbc6dabbe79dc9426d32
ryotokuro <ryotokuro@gmail.com>
2018-08-28 22:38:28 +1000

    Added problem and example

--- algorithms/extraLongFactorials.py
+++ algorithms/extraLongFactorials.py
@@ -1,3 +1,12 @@
 # BACKGROUND
 # The factorial of the integer n, written n!, is defined as:
 #   n! = n * (n-1) * (n-2) * ... * 3 * 2 * 1
+
+# PROBLEM
+# Calculate and print the factorial of a given integer.
+
+# EXAMPLE
+# if n = 30:
+#   - calculate 30 * 29 * 28 * ... * 2 * 1
+#   - we get: 265252859812191058636308480000000
+
