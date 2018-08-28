8de658714e58cc7fffb58417621b0be233a6bece
ryotokuro <ryotokuro@gmail.com>
2018-08-28 23:59:47 +1000

    added note from challenge

--- algorithms/extraLongFactorials.py
+++ algorithms/extraLongFactorials.py
@@ -15,3 +15,10 @@
 
 # extraLongFactorials has the following parameter(s):
 #   - n: an integer
+
+# NOTE
+# Factorials of  can't be stored even in a  long long variable.
+# Big integers must be used for such calculations.
+# Languages like Java, Python, Ruby etc. can handle big integers, but we need to write additional code in C/C++ to handle huge values.
+# We recommend solving this challenge using BigIntegers.
+
