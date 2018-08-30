bf9f56819f12b6e6e6962f3ae1e72b32648cf60f
ryotokuro <ryotokuro@gmail.com>
2018-08-30 23:46:10 +1000

    added function call,fixed some formatting

--- algorithms/extraLongFactorials.py
+++ algorithms/extraLongFactorials.py
@@ -12,17 +12,19 @@
 
 # FUNCTION DESCRIPTION
 # Complete the extraLongFactorials function in the editor below. It should print the result and return.
-def extraLongFactorials():
+def extraLongFactorials(integer):
 
     return 1
+
 
 # extraLongFactorials has the following parameter(s):
 #   - n: an integer
 integer = int(input())
 
+extraLongFactorials(integer)
 # NOTE
 # Factorials of  can't be stored even in a  long long variable.
 # Big integers must be used for such calculations.
-# Languages like Java, Python, Ruby etc. can handle big integers, but we need to write additional code in C/C++ to handle huge values.
-# We recommend solving this challenge using BigIntegers.
-
+# Languages like Java, Python, Ruby etc. can handle big integers,
+#   but we need to write additional code in C/C++ to handle huge values.
+# RECOMMENDED: solve this challenge using BigIntegers.
