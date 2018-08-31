e3d92f5956dae926f4446970e3b752f95ddc73e0
ryotokuro <ryotokuro@gmail.com>
2018-08-31 12:33:40 +1000

    SOLVED: link added, fixed formatting (CLOSED)

--- algorithms/extraLongFactorials.py
+++ algorithms/extraLongFactorials.py
@@ -1,3 +1,5 @@
+# SOLVED: https://www.hackerrank.com/challenges/extra-long-factorials/
+
 # BACKGROUND
 # The factorial of the integer n, written n!, is defined as:
 #   n! = n * (n-1) * (n-2) * ... * 3 * 2 * 1
@@ -9,6 +11,7 @@
 # if n = 30:
 #   - calculate 30 * 29 * 28 * ... * 2 * 1
 #   - we get: 265252859812191058636308480000000
+
 
 # FUNCTION DESCRIPTION
 # Complete the extraLongFactorials function in the editor below. It should print the result and return.
@@ -23,7 +26,7 @@ def extraLongFactorials(integer):
 #   - n: an integer
 integer = int(input())
 
-extraLongFactorials(integer)
+print(extraLongFactorials(integer))
 # NOTE
 # Factorials of  can't be stored even in a  long long variable.
 # Big integers must be used for such calculations.
