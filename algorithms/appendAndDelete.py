5ee668755518cb51445f77816fcfaef6ad8184df
ryotokuro <ryotokuro@gmail.com>
2018-09-02 23:55:24 +1000

    added problem/task

---
+++ algorithms/appendAndDelete.py
@@ -0,0 +1,5 @@
+# PROBLEM
+# You have a string of lowercase English alphabetic letters. You can perform two types of operations on the string:
+
+#   1. Append a lowercase English alphabetic letter to the end of the string.
+#   2. Delete the last character in the string. Performing this operation on an empty string results in an empty string.
