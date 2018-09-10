fb63ce93cb0e687808b38d7ad6a9eb52aba21acf
ryotokuro <ryotokuro@gmail.com>
2018-09-10 23:55:18 +1000

    added background on problem

---
+++ algorithms/sherlockSquares.py
@@ -0,0 +1,9 @@
+# BACKGROUND
+# Watson likes to challenge Sherlock's math ability.
+# He will provide a starting and ending value describing a range of integers.
+# Sherlock must determine the number of square integers within that range, inclusive of the endpoints.
+
+# NOTE
+# A square integer is an integer which is the square of an integer, e.g. 1, 4, 9, 16, 25
+
+# EXAMPLE, the range is a =24 and , inclusive. There are three square integers in the range:  and .
