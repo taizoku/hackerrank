058337acbe7f96af67001bdec4fbfb0d53c2d11a
ryotokuro <ryotokuro@gmail.com>
2018-09-14 23:52:25 +1000

    previous commit ignored case where there could be ONE perfect square in the range; issue may just be 0 and 1 edge case error

--- algorithms/sherlockSquares.py
+++ algorithms/sherlockSquares.py
@@ -31,7 +31,7 @@ def squares(lower, upper):
         if not((math.sqrt(upper)).is_integer()):  # if upper is NOT a perfect square
             upper -= 1  # move backwards
 
-    if math.sqrt(lower).is_integer() and math.sqrt(upper).is_integer() and upper > lower:
+    if lower != 0 and lower != 1 and upper > lower:
         numSq = int(math.sqrt(upper) - math.sqrt(lower)) + 1
     return numSq
 
