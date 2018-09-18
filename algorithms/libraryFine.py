86ad6f3f02e533c6db3b07d2670788b756a4ed60
ryotokuro <ryotokuro@gmail.com>
2018-09-18 17:39:56 +1000

    added function implementation and such

--- algorithms/libraryFine.py
+++ algorithms/libraryFine.py
@@ -20,15 +20,38 @@
 # Whether a book is due January 1, 2017 or December 31, 2017, if it is returned January 1, 2018,
 # that is a year late and the fine would be 10, 000 Hackos
 
+
 # FUNCTION DESCRIPTION
 # Return an integer representing the fine due.
 
 # libraryFine has the following parameter(s):
 #   - d1, m1, y1: returned date day, month and year
 #   - d2, m2, y2: due date day, month and year
+def libraryFine(returnDay, returnMonth, returnYear, dueDay, dueMonth, dueYear):
+    fine = 0 # if book returned on or before expected due date
+    if returnYear > dueYear:
+        fine = 10000 # Hackos
+
+    elif returnYear == dueYear:
+        if returnMonth > dueMonth:
+            fine = 500
+
+        elif returnMonth == dueMonth:
+            if returnDay > dueDay:
+                fine = 15 * (returnDay - dueDay)
+
+    return fine
+
 
 # INPUT FORMAT
 # 1. Contains 3 space-separated integers, d1, m1, y1
 #       denoting the respective day, month and year on which the book was returned.
+returnDay, returnMonth, returnYear = map(int, input().strip().split(' '))
+
 # 2. The second line contains 3 space-separated integers, d2, m2, y2
 #       denoting the respective day, month and year on which the book was due to be returned.
+dueDay, dueMonth, dueYear = map(int, input().strip().split(' '))
+
+# OUTPUT FORMAT
+# Print a single integer denoting the library fine for the book received as input.
+print(libraryFine(returnDay, returnMonth, returnYear, dueDay, dueMonth, dueYear))
