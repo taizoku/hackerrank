df68ea3775aaa0a6bf7b266b5ac81cb22d92d89b
ryotokuro <ryotokuro@gmail.com>
2019-01-11 19:48:51 +1100

    added new problem with description

---
+++ algorithms/cloudJumping2.py
@@ -0,0 +1,12 @@
+# Aerith is playing a cloud hopping game.
+# In this game, there are sequentially numbered clouds that can be thunderheads or cumulus clouds.
+# Her character must jump from cloud to cloud until it reaches the start again.
+
+# To play, Aerith is given an array of clouds, c and an energy level e = 100.
+# She starts from  and uses  unit of energy to make a jump of size  to cloud . If Aerith lands on a thundercloud, , her energy () decreases by additional units. The game ends when Aerith lands back on cloud .
+
+# Given the values of:
+# - n
+# - k
+# - the configuration of the clouds as an array
+# can you determine the final value of  after the game ends?
