f6707bbcb62add7ba38f7e55ae497c002299c115
ryotokuro <ryotokuro@gmail.com>
2019-01-15 23:20:41 +1100

    added example, set layout of code

--- algorithms/cloudJumping2.py
+++ algorithms/cloudJumping2.py
@@ -17,3 +17,18 @@
 # - k
 # - the configuration of the clouds as an array
 # Can you determine the final value of e after the game ends?
+
+# EXAMPLE:
+# c = [0, 0, 1, 0]
+# k = 2
+# the indices of her path are 0 -> 2 -> 0
+
+# Her energy level decreases by 1 for each jump to 98
+# She landed on ONE thunderhead = additional cost of 2 energy units
+# Therefore, her final energy level is 96
+
+# FUNCTION
+
+# INPUT
+
+# OUTPUT (e)
