aa5b33258c46725c63c25963dc727ee8f3f3600e
ryotokuro <ryotokuro@gmail.com>
2019-01-18 10:46:18 +1100

    rewriting a new approach to the problem

--- algorithms/appendAndDelete.py
+++ algorithms/appendAndDelete.py
@@ -24,7 +24,42 @@
 
 # FUNCTION DESCRIPTION
 # Function should return a string, either Yes or No.
-def appendAndDelete(initialString, finalString, numOperations):
+def appendAndDelete(initString, finalString, numOperations):
+    same = "No"
+    i = None
+
+    while numOperations > 0:
+        if finalString == initString:
+            same = "Yes"
+            break
+
+        if len(initString) > finalString:  # reduce first string to the same length as the second
+            initString.pop()  # delete the last character until they are the same length
+            numOperations -= 1
+            continue  # should count as an operation, so it skips to the next count of the loop
+
+        # once they're the same length, now we want to check up to what letters are the same
+        if i is not None:
+            for i in len(initString):
+                if initString[i] != finalString[i]:
+                    break  # loop until you reach the mismatching letter
+
+        # now we need to continue to pop off characters until they are the same
+        # should only be done once procedurally
+        while len(initString) != i+1:
+            initString.pop()
+            numOperations -= 1
+
+        # then we need to add characters so they're identical
+        if len(initString) != len(finalString):
+            initString.append(finalString[i])
+            i += 1
+
+    if initString == finalString:
+        string = "Yes"
+
+    return same
+
     string = "No"
     # plan is to iterate through the string and compare each letter
     # if equal, return "Yes"
