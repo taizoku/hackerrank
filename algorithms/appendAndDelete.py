352a8a5cb7ba501e61fadd4538423b578d64d6fd
ryotokuro <ryotokuro@gmail.com>
2019-01-17 22:19:42 +1100

    trying to fix some past code

--- algorithms/appendAndDelete.py
+++ algorithms/appendAndDelete.py
@@ -1,10 +1,16 @@
 # PROBLEM
-# You have a string of lowercase English alphabetic letters. You can perform two types of operations on the string:
+# Given a string of lowercase English alphabetic letters.
+# You can perform two types of operations on the string:
 #   1. Append a lowercase English alphabetic letter to the end of the string.
-#   2. Delete the last character in the string. Performing this operation on an empty string results in an empty string.
+#   2. Delete the last character in the string.
+#   **Performing this operation on an empty string results in an empty string.
 #
-# Given an integer, k, and two strings, s and t, determine whether or not you can convert s to t
-# by performing exactly k of the above operations on s. If it's possible, print Yes. Otherwise, print No.
+# Given:
+# - integer k,
+# - strings s
+# - string t
+# -> Determine whether or not you can convert s to t by performing exactly k of the above operations on s.
+# If it's possible, print Yes. Otherwise, print No.
 
 # EXAMPLE
 # strings s = [a, b, c] and t = [d, e, f].
