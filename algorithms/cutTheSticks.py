5227324d63a1cc1ddbe2ce32fe1b20d715721858
ryotokuro <ryotokuro@gmail.com>
2019-01-30 14:12:13 +1100

    added problem statement

---
+++ algorithms/cutTheSticks.py
@@ -0,0 +1,7 @@
+# Link: https://www.hackerrank.com/challenges/cut-the-sticks/problem
+
+# PROBLEM
+# You are given a number of sticks of varying lengths. You will iteratively cut the sticks into smaller sticks,
+# discarding the shortest pieces until there are none left. At each iteration you will determine the length of
+# the shortest stick remaining, cut that length from each of the longer sticks and then discard all the pieces
+# of that shortest length. When all the remaining sticks are the same length, they cannot be shortened so discard them.
