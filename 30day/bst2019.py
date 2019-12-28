6f27b14f0a6783660e36372ab4a3923135ea19ed
ryotokuro <ryotokuro@gmail.com>
2019-12-28 23:37:20 +1100

    back trying to finish this section again

---
+++ 30day/bst2019.py
@@ -0,0 +1,28 @@
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        queue = []
+        #Write your code here
+        if root:  # root not empty
+            queue.append(root)
+
+        while queue:  # while queue is not empty            
+
+T=int(input())
