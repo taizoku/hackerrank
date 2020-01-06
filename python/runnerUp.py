ce56d756451673202e53f1a867e66df12eedff1f
ryotokuro <ryotokuro@gmail.com>
2020-01-06 13:27:12 +1100

    renamed 30 day challenges, adding files from python section

---
+++ python/runnerUp.py
@@ -0,0 +1,8 @@
+"""
+https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    print(sorted(set(arr))[-2])
