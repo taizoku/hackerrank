c592e58124f8e649b0d864f53667f79ec39759f2
ryotokuro <ryotokuro@gmail.com>
2020-01-07 13:09:11 +1100

    made input more efficient, added comments on logic

--- python/nestedLists.py
+++ python/nestedLists.py
@@ -2,22 +2,18 @@
 https://www.hackerrank.com/challenges/nested-list/problem
 """
 
-n = int(input())
-students = []
-for i in range(n):
+students = []  # store student lists: [name, grade]
+for _ in range(int(input()):
     name = input()
     grade = float(input())
     students.append([name, grade])
 
-students.sort(key=lambda x: x[1])  # sorts by grade
+students.sort(key=lambda x: x[1])  # sort students by grade
 
 grades = sorted(set([i[1] for i in students]))  # gives me grades as a set sorted lowest to highest
-key = grades[1]
+key = grades[1]  # identify second lowest grade
 
-answer = sorted([i[0] for i in students if i[1] == key])
+answer = sorted([i[0] for i in students if i[1] == key])  # find students with second lowest grade and sort alphabetically
 
 for i in answer:
-    print(i)
-
-
-
+    print(i)  # now print students
