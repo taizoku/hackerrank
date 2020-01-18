5bc254dba8cef9fe161dbaea8860ec7385003bfc
ryotokuro <ryotokuro@gmail.com>
2020-01-18 16:53:29 +1100

    fixed solution for day 28, regex filter

--- 30day/28. regex.py
+++ 30day/28. regex.py
@@ -2,14 +2,18 @@ import re
 
 n = int(input())
 
+text = ""
 for _ in range(n):
-    
+    text += input()
+    text += '\n'
+
+print(text)
 
 # filter for gmails only
-pattern = re.compile('([a-z]{1,20})\s([a-z]{1,40}@gmail\.com)')
+pattern = re.compile('([a-z]{1,20})\s([a-z@\.]{1,40}@gmail\.com)')
 
 # find emails out of given input
-matches = pattern.findall('riya riya@gmail.com julia julia@julia.me julia sjulia@gmail.com')
+matches = pattern.findall(text)
 
 # sort alphabetically
 names = sorted([i[0] for i in matches])
