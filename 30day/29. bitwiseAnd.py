1d5be79c7a507e7d79e6e9a3c1f867fce002f2de
ryotokuro <ryotokuro@gmail.com>
2020-01-19 23:57:03 +1100

    added strategy

--- 30day/29. bitwiseAnd.py
+++ 30day/29. bitwiseAnd.py
@@ -1,12 +1,15 @@
 # number of test cases
 t = int(input())
+tests = {}
 
 for _ in range(t):
     # get space separated input
     # 1. n - defines set length S where S = {1, 2, 3, ... n}
     # 2. k - maximum size of number
     n, k = map(int, input().split())
-
+    tests[n] = k
+print(sorted(tests))
+    """
     answer = None
     for i in range(1, n):
         for j in range(i+1, n+1):
@@ -14,4 +17,12 @@ for _ in range(t):
             if i&j > answer and i&j < k:
                 answer = i&j
     print(answer)
-    
+    """
+
+# instead of doing it in real-time
+# store the test numbers first
+
+# then compute results for largest number (e.g. 999 is one test so nest for loop up to 999)
+# then go through stored test numbers (loop) and slice the dictionary
+# do a lookup for largest number smaller than k
+# print answer
